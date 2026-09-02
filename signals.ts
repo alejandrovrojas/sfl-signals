7dd0a79869acd13604b1157948853264eb345114
Alejandro V. Rojas <6848068+alejandrovrojas@users.noreply.github.com>
2026-09-02 19:22:04 +0200

    Add SignalElement class
    - Custom class that wraps all its non-global attribute values as signal
    values
    
    - The class depends on a signal implementation and therefore makes sense
    to bundle it with the rest of the library

--- signals.ts
+++ signals.ts
@@ -182,7 +182,7 @@ export class Computed<T> extends Signal<T> implements ReadonlySignal<T> {
 		return value;
 	}
 
-	public set value(v: T) {
+	public set value(_: T) {
 		throw new Error('Computed signals are readonly');
 	}
 }
@@ -306,5 +306,150 @@ export function batch<T>(callback: () => T): T {
 		}
 	} else {
 		return callback();
+	}
+}
+
+// extend a custom element by mapping all non-global attributes to DOM properties
+// of the instance, where the value is wrapped as a signal. attributes are kept in
+// sync with the signal value as strings
+export class SignalElement extends HTMLElement {
+	// add an internal check for the first time the component is
+	// inserted in the DOM. this allows for static definition of attributes
+	// that only get converted to signals when inserted the first time.
+	// the fact that this does not re-run on subsequent connectedCallback
+	// calls (when a DOM item is moved, reordered, etc.) also keeps the
+	// initialized instance and its signals in memory
+	//
+	//     e.g.
+	//
+	//     class MySignalElement extends SignalElement {
+	//         declare custom: string;  // declare beforehand for correct typechecking
+	//     }
+	//
+	//     const element = document.createElement('custom-element');
+	//     element.setAttribute('custom', 'value');
+	//
+	//     body.appendChild(element);   // element is now initialized
+	//
+	//     element                      // MySignalElement
+	//     element.custom.value         // Signal<string>;
+	initialized = false;
+
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		if (!this.initialized) {
+			// iterate through all defined html attributes (except for global attributes) and define
+			// them as DOM properties with signals as values. the initial string value taken from the html
+			// attribute is cast as a literal value. any subsequent assignments to the signal value will
+			// set the value of the corresponding html attribute. this is useful for simple two-way data
+			// binding of booleans, numbers, etc. though not for objects, as setAttribute() always converts
+			// the value back to string format.
+			//
+			//     e.g.
+			//
+			//     <my-custom-element foo-bar="baz">
+			//
+			//     class MySignalElement extends SignalElement {
+			//        init() {
+			//           this.foo_bar.value;          // -> 'baz'
+			//           this.foo_bar.value = 'qux'   // -> <my-custom-element foo-bar="qux">
+			//        }
+			//     }
+			const global_attributes = [
+				'accesskey',
+				'class',
+				'contenteditable',
+				'dir',
+				'draggable',
+				'enterkeyhint',
+				'hidden',
+				'id',
+				'inert',
+				'inputmode',
+				'lang',
+				'popover',
+				'role',
+				'spellcheck',
+				'style',
+				'tabindex',
+				'title',
+				'translate',
+			];
+
+			for (const attribute_name of this.getAttributeNames()) {
+				const attribute_property_name = attribute_name.replace(/-/g, '_'); // hypen-case to snake_case
+				const attribute_initial_value = this.getAttribute(attribute_name)!;
+				const is_attribute_global = global_attributes
+					.map(
+						attr =>
+							attribute_name === attr ||
+							attribute_name.startsWith('data-') ||
+							attribute_name.startsWith('aria-')
+					)
+					.some(attr => attr === true);
+
+				if (!is_attribute_global) {
+					(this as any)[attribute_property_name] = new SignalElementAttributeSignal(
+						attribute_initial_value,
+						attribute_name,
+						this
+					);
+				}
+			}
+
+			this.init();
+		}
+
+		this.initialized = true;
+	}
+
+	init() {}
+}
+
+// use a class to wrap the signal value of a custom attribute, and cast the initial attribute
+// (string) value to a number/boolean/null if necessary. getting property.value accesses the internal
+// signal.value, and setting a property.value assigns signal.value and sets the corresponding
+// HTML attribute value as string
+class SignalElementAttributeSignal {
+	declare current_value:  Signal<unknown>;
+	declare name:           string;
+	declare element:        SignalElement;
+
+	constructor(
+		attribute_initial_value: string,
+		attribute_name:          string,
+		element:                 SignalElement
+	) {
+		this.current_value = signal(this.cast_attribute_value(attribute_initial_value));
+		this.name          = attribute_name;
+		this.element       = element;
+	}
+
+	cast_attribute_value(initial_value: string) {
+		switch (initial_value) {
+			case '':
+			case 'null':
+			case 'undefined':
+				return null;
+			case 'true':
+				return true;
+			case 'false':
+				return false;
+			default:
+				const n = +initial_value; // +value is the fastest numeric coercion
+				return Number.isNaN(n) ? initial_value : n;
+		}
+	}
+
+	get value() {
+		return this.current_value.value;
+	}
+
+	set value(new_value: unknown) {
+		this.current_value.value = new_value;
+		this.element.setAttribute(this.name, String(new_value));
 	}
 }
